0ac58fa485302e92a371ce550b94c79f049175a9
Olga Kurkina <doktorovaoe@gmail.com>
2023-02-16 10:36:07 +0300

    Sprint7_mesto_v2.5

---
+++ scripts/Card.js
@@ -0,0 +1,57 @@
+class Card {
+	constructor(template, item, openPopupImg) {
+		this._template = template;
+		this._name = item.name;
+		this._link = item.link;
+		this._element = this._getTemplate();
+		this._cardImage = this._element.querySelector('.element__img');
+		this._cardTitile = this._element.querySelector('.element__name');
+		this._openPopupImg = openPopupImg;
+		this._likeButton = this._element.querySelector('.element__like');
+		this._deleteButton = this._element.querySelector('.element__trash');
+	}
+
+	//получаем разметку
+	_getTemplate() {
+		const cardElement = document.querySelector(this._template)
+			.content
+			.querySelector('.element')
+			.cloneNode(true);
+		return cardElement;
+	}
+
+	//создаем карточку
+	generateCard() {
+		this._cardTitile.textContent = this._name;
+		this._cardImage.src = this._link;
+		this._cardImage.alt = this._name;
+		this._setEventListeners();
+		return this._element;
+	}
+
+	//метод удаления карточки
+	_deleteCard() {
+		this._element.remove();
+	}
+
+	//метод установка лайка
+	_likeCard() {
+		this._likeButton.classList.toggle('element__like_active');
+	}
+
+	//слушатели событый
+	_setEventListeners() {
+		this._deleteButton.addEventListener('click', () => {
+			this._deleteCard();
+		});
+		this._likeButton.addEventListener('click', () => {
+			this._likeCard();
+		});
+		this._cardImage.addEventListener('click', () => {
+			this._openPopupImg(this._name, this._link);
+		});
+
+	};
+}
+
+export default Card
